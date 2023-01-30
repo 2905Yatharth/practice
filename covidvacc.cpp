9060edc96a9ed5cc63afcc8e8684cf2ce062febd
Yatharth Sharma <104185538+2905Yatharth@users.noreply.github.com>
2023-01-30 23:49:22 +0530

    Update covidvacc.cpp

--- covidvacc.cpp
+++ covidvacc.cpp
@@ -1,9 +1,39 @@
 // some changes are required 
+// works fine
 
-int maxVaccinesAdministered(int n, int dayNumber, int maxVaccines)
+int solve(int mid, int left, int right)
 {
-    int left = dayNumber;
-    int right = n - 1 - dayNumber;
+    int sum = mid * mid;
+    
+    if (mid - 1 >= left)
+    {
+        int k = mid - 1 - left;
+        sum -= k * (k + 1) / 2;
+    }
+    
+    else
+    {
+        sum += left - mid + 1;
+    }
+
+    if (mid - 1 >= right)
+    {
+        int k = mid - 1 - right;
+        sum -= k * (k + 1) / 2;
+    }
+    
+    else
+    {
+        sum += right - mid + 1;
+    }
+
+    return sum;
+}
+
+int maxVaccinesAdministered(int n, int dayNum, int maxVaccines)
+{
+    int left = dayNum;
+    int right = n - 1 - dayNum;
     int start = 1;
 
     int end = maxVaccines;
